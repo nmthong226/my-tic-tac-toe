16b1bf25fe23006c1f50c1719a464d7aeb18698c
nmthong226 <nmthong226@gmail.com>
2024-10-17 23:27:02 +0700

    ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react';
+import { FaCircleNotch } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
 import './App.css';
 
-function Square({ value, onSquareClick }) {
+const cx = (...classes) => classes.filter(Boolean).join(' ');
+
+function Square({ value, onSquareClick, className }) {
   return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
+    <button className={cx("flex items-center justify-center square", className)} onClick={onSquareClick}>
+      <div className="w-[90%] h-[90%] flex items-center justify-center hover:bg-white rounded-3xl hover:scale-110 transform transition-transform ease-out duration-200">
+        {value === 'X' ?
+          <FaCircleNotch className="text-red-500 w-20 h-20" /> :
+          value === 'O' ?
+            <FaXmark className="text-blue-500 w-28 h-28" /> : null
+        }
+      </div>
     </button>
   );
 }
 
-function Board({ xIsNext, squares, onPlay, currentMove }) {
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -33,9 +43,8 @@ function Board({ xIsNext, squares, onPlay, currentMove }) {
 
   return (
     <>
-      <div className="status">{status}</div>
       {Array(3).fill(null).map((_, rowIndex) => (
-        <div className="board-row" key={rowIndex}>
+        <div className="board-row grid grid-cols-3 h-full w-full" key={rowIndex}>
           {Array(3).fill(null).map((_, colIndex) => {
             const index = rowIndex * 3 + colIndex;
             return (
@@ -43,9 +52,15 @@ function Board({ xIsNext, squares, onPlay, currentMove }) {
                 key={index}
                 value={squares[index]}
                 onSquareClick={() => handleClick(index)}
-                currentMove
-              />
-            )
+                className={`w-full h-full flex items-center justify-center bg-indigo-100 
+                  ${rowIndex < 2 ? 'border-b-4' : ''} 
+                  ${colIndex < 2 ? 'border-r-4' : ''}
+                  ${rowIndex == 0 && colIndex == 0 ? 'rounded-tl-3xl' : ''}
+                  ${rowIndex == 0 && colIndex == 2 ? 'rounded-tr-3xl' : ''} 
+                  ${rowIndex == 2 && colIndex == 0 ? 'rounded-bl-3xl' : ''} 
+                  ${rowIndex == 2 && colIndex == 2 ? 'rounded-br-3xl' : ''} 
+                  border-white rounded-none hover:border-white p-2 focus:outline-none`} />
+            );
           })}
         </div>
       ))}
@@ -80,7 +95,7 @@ export default function Game() {
     return (
       <li key={move}>
         {checkMove ? (
-          <p>{ description }</p>
+          <p>{description}</p>
         ) : (
           <button onClick={() => jumpTo(move)}>{description}</button>
         )}
@@ -89,15 +104,16 @@ export default function Game() {
   });
 
   return (
-    <div className="game">
-      <div className="game-board">
+    <div className="game flex flex-row w-screen h-screen justify-center items-center">
+      <div className="game-board flex flex-col w-[500px] h-[500px] bg-indigo-100 items-center justify-center rounded-3xl border-[1px] border-white p-4 relative">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
-      <div className="game-info">
-        <button>Ascending order</button>
-        <ol>{moves}</ol>
+        <div className="game-info absolute -right-64 bg-indigo-50 h-[500px] w-56 rounded-3xl">
+          <button className="mb-4">Ascending order</button>
+          <ol>{moves}</ol>
+        </div>
       </div>
     </div>
+
   );
 }
 
